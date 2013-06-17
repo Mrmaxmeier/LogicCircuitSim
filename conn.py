b145d625f4871d93dace8082cbccc53ad1e40e57
felix <felixbauckholt AT gmail PUNKT com>
2013-06-17 17:08:26 +0200

    Wenn im del-Modus auf eine Verbindung geclickt wird, wird diese gelöscht!

--- conn.py
+++ conn.py
@@ -20,4 +20,12 @@ class Connection(object):
 		inp, inpName = self.input
 		x2, y2 = getPoint(inp.frame.inLabels[inpName])
 		print( x1, y1, x2, y2)
-		return canvas.create_line(x1, y1, x2, y2, fill=standardCols(self.bool), width=3)
+		id = canvas.create_line(x1, y1, x2, y2, fill=standardCols(self.bool), width=3)
+		canvas.tag_bind(id, "<ButtonPress>", self.click)
+		return id
+	
+	def click(self, ev):
+		from sim import Simulator as S
+		if S.sim.selectedTool == "del":
+			print("conn clicked!", S.sim.selectedTool)
+			S.sim.connections.remove(self)
